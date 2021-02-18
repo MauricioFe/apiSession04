a3366d055bb7be028add0eeb55cd8682d1ecaabc
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-02-18 17:02:29 -0300

    Criando os models

---
+++ apiSession04/apiSession04/Controllers/RelatosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiSession04.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RelatosController : Controller
+    {
+
+    }
+}
---
+++ apiSession04/apiSession04/Model/Funcao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiSession04.Model
+{
+    public class Funcao
+    {
+        public int Id { get; set; }
+        public string funcao { get; set; }
+    }
+}
