3b4d1eeb83f9e877fdadec4f2074748aa5a4257a
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-02-18 17:13:22 -0300

    Criando o endpoint para retornar os usuarios

--- apiSession04/apiSession04/Controllers/UsuariosController.cs
+++ apiSession04/apiSession04/Controllers/UsuariosController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using apiSession04.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +14,33 @@ namespace apiSession04.Controllers
     [ApiController]
     public class UsuariosController : ControllerBase
     {
+        SqlConnection conn = new SqlConnection(@"Data source= .\SQLEXPRESS; Initial Catalog=SessaoMobile; Integrated Security=true");
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        DataTable dt;
+
+        [HttpGet]
+        public IEnumerable<Usuario> GetUsuarios()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            try
+            {
+                cmd = new SqlCommand("Select * From Usaurio inner join funcao on funcao.id =  usuario.funcaoid", conn);
+                conn.Open();
+                adapter = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                adapter.Fill(dt);
+                foreach (var item in dt.Rows)
+                {
+
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return usuarios;
+        }
     }
 }
