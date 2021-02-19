68aa349f8fdc80ce32183d521c64023a80e00189
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-02-19 08:44:50 -0300

    Configurando url

--- apiSession04/apiSession04/Program.cs
+++ apiSession04/apiSession04/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace apiSession04
@@ -19,6 +20,7 @@ namespace apiSession04
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseUrls($"http://{Dns.GetHostAddresses(Dns.GetHostName()).Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).First()}:5000")
                 .UseStartup<Startup>();
     }
 }
